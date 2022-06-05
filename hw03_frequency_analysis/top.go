5df37720b867db2e7f3d7ba40240860be5d36d17
Skeltis <Morfenume@yandex.ru>
2022-06-05 22:30:25 +0300

    HW3 is done

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,65 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var (
+	counterDictionary map[string]int
+	searcher          = regexp.MustCompile(`\s\-\s|[\s.;:,\!\?\\\/()#^%@'"` + "`" + `~+=*]+`)
+)
+
+func Top10(input string) []string {
 	// Place your code here.
-	return nil
+	counterDictionary = make(map[string]int)
+	rawWords := searcher.Split(input, -1)
+	array := packIntoWordFreqsArray(rawWords)
+	sortWordFrequencies(array)
+	totalRecords := len(array)
+	if totalRecords > 10 {
+		totalRecords = 10
+	}
+	top10 := make([]string, totalRecords)
+	for index, pair := range array[0:totalRecords] {
+		top10[index] = pair.word
+	}
+	return top10
+}
+
+func packIntoWordFreqsArray(rawWords []string) []wordFrequency {
+	for _, word := range rawWords {
+		if word != "" {
+			lowerWord := strings.ToLower(word)
+			counterDictionary[lowerWord]++
+		}
+	}
+	wordFreqs := make([]wordFrequency, len(counterDictionary))
+	index := 0
+	for key, value := range counterDictionary {
+		wordFreqs[index] = wordFrequency{
+			word:  key,
+			count: value,
+		}
+		index++
+	}
+	return wordFreqs
+}
+
+func sortWordFrequencies(wordFreqs []wordFrequency) {
+	sort.Slice(wordFreqs, func(i, j int) bool {
+		if wordFreqs[i].count > wordFreqs[j].count {
+			return true
+		}
+		if wordFreqs[i].count < wordFreqs[j].count {
+			return false
+		}
+		return wordFreqs[i].word < wordFreqs[j].word
+	})
+}
+
+type wordFrequency struct {
+	word  string
+	count int
 }
