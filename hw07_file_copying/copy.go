36fe6ecf3edd9e6c252dfb0de88e47be46497696
Skeltis <Morfenume@yandex.ru>
2022-07-24 19:48:00 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/schollz/progressbar/v3"
 )
 
 var (
@@ -11,5 +16,67 @@ var (
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
 	// Place your code here.
+	return copyInternal(fromPath, toPath, offset, limit)
+}
+
+func copyInternal(sourceFilePath, targetFilePath string, offset, limit int64) error {
+	sourceFile, err := os.Open(sourceFilePath)
+	if err != nil {
+		return fmt.Errorf("error while openning file: %w", err)
+	}
+	defer sourceFile.Close()
+
+	actualLimit, err := checkSourceCountLimit(sourceFile, offset, limit)
+	if err != nil {
+		return err
+	}
+
+	targetFile, err := os.Create(targetFilePath)
+	if err != nil {
+		return err
+	}
+	defer targetFile.Close()
+
+	// actual counted limit was 0, so we only create empty file
+	if actualLimit == 0 {
+		return nil
+	}
+
+	if offset != 0 {
+		_, err = sourceFile.Seek(offset, 0)
+		if err != nil {
+			return fmt.Errorf("error while moving file cursor: %w", err)
+		}
+	}
+
+	progressBar := progressbar.Default(actualLimit)
+
+	_, err = io.CopyN(io.MultiWriter(targetFile, progressBar), sourceFile, actualLimit)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return fmt.Errorf("error while copying %s to %s: %w", sourceFilePath, targetFilePath, err)
+	}
+
 	return nil
+}
+
+func checkSourceCountLimit(file *os.File, offset, limit int64) (int64, error) {
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return 0, fmt.Errorf("error reading file info: %w", err)
+	}
+
+	if !fileInfo.Mode().IsRegular() {
+		return 0, ErrUnsupportedFile
+	}
+
+	fileSize := fileInfo.Size()
+	if offset > fileSize {
+		return 0, ErrOffsetExceedsFileSize
+	}
+
+	if limit == 0 || limit > fileSize-offset {
+		return fileSize - offset, nil
+	}
+
+	return limit, nil
 }
--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -1,3 +1,8 @@
-module github.com/fixme_my_friend/hw07_file_copying
+module github.com/skeltis/otus_go_hw/hw07_file_copying
 
 go 1.16
+
+require (
+	github.com/schollz/progressbar/v3 v3.8.7
+	github.com/stretchr/testify v1.8.0
+)
